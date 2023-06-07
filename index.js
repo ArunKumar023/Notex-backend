df17e2bf608f10dcdfc508d64cde13bce7955312
Arun kumar <ak2965491@gmail.com>
2023-06-07 13:59:20 +0530

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express")
+
+
 const app = express();
 const port = process.env.PORT || 7000;
 
+
+app.get("/",(req, res)=>{
+    res.send("this is server merge frontend")
+})
 
 
 
