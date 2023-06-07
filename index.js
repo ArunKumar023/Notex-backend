eda2f103024d652632fc4f72ff72b41a720c8062
Arun kumar <ak2965491@gmail.com>
2023-06-07 14:34:04 +0530

    first commit

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const express = require("express")
-
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 7000;
+const noteRouter = require('./routes/Notes');
 
 
-app.get("/",(req, res)=>{
-    res.send("this is server merge frontend");
-});
+app.use(cors());
+app.use(express.json()); 
+app.use('/', noteRouter);
 
+// app.get("/",(req, res)=>{
+//     res.send("this is server merge frontend");
+// });
+
+const uri = "mongodb+srv://ak12345:ak12345@cluster0.fbxzg0o.mongodb.net/Notex?retryWrites=true&w=majority";
+// const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true });
+
+const connection = mongoose.connection;
+connection.once('open',  () => {
+    console.log('MongoDB connection established');
+})
 
 
 app.listen(port, () => {
---
+++ models/note.models.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const noteSchema = new mongoose.Schema ({
+    Title: String,
+    Content: String,
+    // Email: String,
+}, {
+    timestamps: true,
+});
+
+const Note = mongoose.model('Note', noteSchema);
+module.exports = Note;
